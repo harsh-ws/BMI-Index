c53d6dcf3de5d6e6773508389b45f86808507bfe
Harsh <hackwidharsh@gmail.com>
2022-11-30 20:41:49 +0530

    added the slider & relevant UI changes

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kCardColor = Color(0xFF1D1E33);
+const kInActiveColor = Color(0xFF111328);
+const kActiveColor = Color(0xFF1D1E33);
+
+const kLabelTextStyle = TextStyle(
+  color: Colors.white,
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 60,
+  fontWeight: FontWeight.w700,
+);
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'constants.dart';
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -9,31 +12,28 @@ class InputPage extends StatefulWidget {
 }
 //0xFF1D1E33
 
-const cardColor = Color(0xFF1D1E33);
-const inActiveColor = Color(0xFF111328);
-const activeColor = Color(0xFF1D1E33);
-
 enum GenderType { male, female }
 
 class _InputPageState extends State<InputPage> {
-  Color maleCardColor = inActiveColor;
-  Color femaleCardColor = inActiveColor;
+  Color maleCardColor = kInActiveColor;
+  Color femaleCardColor = kInActiveColor;
+  int height = 180;
 
   void setColor(GenderType selection) {
     if (selection == GenderType.male) {
       // 1 means male
-      if (maleCardColor == inActiveColor) {
-        maleCardColor = activeColor;
-        femaleCardColor = inActiveColor;
+      if (maleCardColor == kInActiveColor) {
+        maleCardColor = kActiveColor;
+        femaleCardColor = kInActiveColor;
       } else {
-        maleCardColor = inActiveColor;
+        maleCardColor = kInActiveColor;
       }
     } else if (selection == GenderType.female) {
-      if (femaleCardColor == inActiveColor) {
-        femaleCardColor = activeColor;
-        maleCardColor = inActiveColor;
+      if (femaleCardColor == kInActiveColor) {
+        femaleCardColor = kActiveColor;
+        maleCardColor = kInActiveColor;
       } else {
-        femaleCardColor = inActiveColor;
+        femaleCardColor = kInActiveColor;
       }
     }
   }
@@ -116,12 +116,53 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                      child: ReusableCard(
-                    cardChild: Column(
-                      children: <Icon>[Icon(FontAwesomeIcons.cableCar)],
+                    child: Container(
+                      margin: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: femaleCardColor,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Text(
+                            'HEIGHT',
+                            style: kLabelTextStyle,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.baseline,
+                            textBaseline: TextBaseline.alphabetic,
+                            children: <Widget>[
+                              Text(
+                                height.toString(),
+                                style: kNumberTextStyle,
+                              ),
+                              const Text(
+                                'cm',
+                                style: kLabelTextStyle,
+                              )
+                            ],
+                          ),
+                          Slider(
+                            value: height.toDouble(),
+                            onChanged: (newValue) {
+                              setState(() {
+                                height = newValue.round();
+                              });
+                            },
+                            inactiveColor: Colors.white,
+                            activeColor: Colors.green,
+                            min: 80,
+                            max: 220,
+                          ),
+                        ],
+                      ),
                     ),
-                    color: cardColor,
-                  )),
+                  ),
                 ],
               ),
             ),
@@ -130,12 +171,12 @@ class _InputPageState extends State<InputPage> {
                 children: const <Widget>[
                   Expanded(
                     child: ReusableCard(
-                      color: cardColor,
+                      color: kCardColor,
                     ),
                   ),
                   Expanded(
                     child: ReusableCard(
-                      color: cardColor,
+                      color: kCardColor,
                     ),
                   ),
                 ],
@@ -163,7 +204,7 @@ class ReusableCard extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.all(15),
       decoration: BoxDecoration(
-        color: cardColor,
+        color: kCardColor,
         borderRadius: BorderRadius.circular(15),
       ),
     );
